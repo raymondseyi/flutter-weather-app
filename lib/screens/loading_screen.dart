ab6d4f795fe35244f994ad64d0aa33142d3cb874
raymondseyi <seyidami13@gmail.com>
2022-01-19 10:51:18 +0100

    A complete Weather Application for Android and iOS

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_app/screens/location_screen.dart';
+import 'package:flutter_app/services/weather.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    getLocationData();
+    super.initState();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
